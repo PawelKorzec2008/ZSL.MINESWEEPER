425c834a39978d3812a70443bb01f4c70e656633
PawelKorzec2008 <153161756+PawelKorzec2008@users.noreply.github.com>
2024-05-19 20:42:22 +0200

    Delete misiako directory

--- misiako/MainPage/script.js
+++
@@ -1,6 +0,0 @@
-function zakoncz(){
-    window.close('','_parent','');
-}
-function Ustawienia(){
-    location.href = "../Settings/ustawienia.html"
-}
--- misiako/przenoszenie danych temp/script.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-    sessionStorage.setItem("klucz","1") // mozna nadpisywac
-});
